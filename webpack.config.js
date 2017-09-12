2284aa2b9762d48e8cf976ba47fe316648df35e0
juanlmarval <juanluismarval@gmail.com>
2017-09-12 10:50:42 -0400

    Minor changes for github deployment

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './docs'),
-    publicPath: '/dist/',
+    publicPath: '/docs/',
     filename: 'build.js'
   },
   module: {
@@ -33,7 +33,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue'
+      'vue$': 'vue/docs/vue'
     }
   },
   devServer: {
